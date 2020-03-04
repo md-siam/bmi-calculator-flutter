5a8b6ca12395a7458b01b41a135c0d3f13d00bc3
Md. Siam <md.siam03@gmail.com>
2020-03-04 09:10:00 +0600

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'screens/input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: InputPage(),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0D21),
+        scaffoldBackgroundColor: Color(0xFF0A0D21),
+        accentColor: Colors.purple,
+      ),
+    );
+  }
+}
